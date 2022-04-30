3e1a975ec7a28020b67421862569f4155eb96354
harryNeo9 <harishankarpv9@gmail.com>
2022-04-30 12:28:31 +0530

    User JWT authentication

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User' //name of the model object is refering to 
+    },
     text:{
         type:String,
         required: [true, 'Please add a text value']
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,17 +6,14 @@ const {
   updateGoals,
   deleteGoals,
 } = require("../controllers/goalController");
+const {protect} = require('../middleware/authMiddleware')
 
 // router.get("/", getGoals);
-
 // router.post("/", setGoal);
-
-router.route('/').get(getGoals).post(setGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
 
 // router.put("/:id", updateGoals);
-
 // router.delete("/:id", deleteGoals);
-
-router.route('/:id').put(updateGoals).delete(deleteGoals)
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals)
 
 module.exports = router;
